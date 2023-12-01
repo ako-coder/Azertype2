9b0883b64fea8ff6727e63faaea40dd832026fd6
John <johndoe@example.com>
2023-12-01 20:06:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// --- partie 2.2 ---
+
+// const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+// let score = 0
+
+// let motUtilisateur = prompt(`Entrez le mot : ${listeMots[0]}`)
+
+// if (motUtilisateur === listeMots[0]){
+//     score++
+// }
+
+// motUtilisateur = prompt(`Entrez le mot : ${listeMots[1]}`)
+
+// if (motUtilisateur === listeMots[1]){
+//     score++
+// }
+
+// motUtilisateur = prompt(`Entrez le mot : ${listeMots[1]}`)
+
+// if (motUtilisateur === listeMots[2]){
+//     score++
+// }
+    
+// console.log(`Votre score est de : ${score} sur 3`)
+
+
+
+// --- partie 2.3 ---
+
+let i=0;
+
+while(i<3){
+    console.log(i)
+    i++
+}
